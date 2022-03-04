4ebf21ae7976ac7b0d473eb74823694a62c7e90a
aroundthistime <62099715+aroundthistime@users.noreply.github.com>
2022-03-04 14:39:25 +0900

    reactjs project basic setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Router>
+        <Routes />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
