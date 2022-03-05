25554227b51847a30313b6f8ac7cd1ceab0c32f0
aroundthistime <62099715+aroundthistime@users.noreply.github.com>
2022-03-06 02:17:09 +0900

    Social Link - Card Component 활용하는 방식으로 변경, 직접 props 전달

--- src/components/partials/SocialLink/SocialLink.tsx
+++ src/components/partials/SocialLink/SocialLink.tsx
@@ -1,33 +1,26 @@
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import React from 'react';
-import {faGithub} from '@fortawesome/free-brands-svg-icons';
+import Card from '../Card/Card';
 import './socialLink.scss';
-
-interface SocialLinkSubComponents {
-  Default: React.FC<{}>;
-  OnHover: React.FC<{}>;
-}
 
 type Props = {
   to: string;
-  children?: React.ReactNode | React.ReactNode[];
+  title: string;
+  iconElement: React.ReactElement;
+  description: string;
 };
 
-const SocialLink: React.FC<Props> & SocialLinkSubComponents = ({
-  to,
-  children,
-}: Props) => (
-  <a href={to} className="social-link no-drag">
-    {children}
+const SocialLink = ({to, title, iconElement, description}: Props) => (
+  <a href={to} className="social-link">
+    <Card>
+      <Card.Default>
+        {title}
+        {React.cloneElement(iconElement, {
+          className: `${iconElement.props.className} social-link__icon`,
+        })}
+      </Card.Default>
+      <Card.OnHover>{description}</Card.OnHover>
+    </Card>
   </a>
-);
-
-SocialLink.Default = ({children}) => (
-  <div className="social-link__card social-link__card--default">{children}</div>
-);
-
-SocialLink.OnHover = ({children}) => (
-  <div className="social-link__card social-link__card--hidden">{children}</div>
 );
 
 export default SocialLink;
--- src/components/partials/SocialLinks/SocialLinks.tsx
+++ src/components/partials/SocialLinks/SocialLinks.tsx
@@ -10,35 +10,21 @@ const SocialLinks = () => (
     <Section.Title>Social Links</Section.Title>
     <Section.Content>
       <ul className="social-links">
-        <GithubLink />
-        <BlogLink />
+        <SocialLink
+          to="https://github.com/aroundthistime"
+          title="Github"
+          iconElement={<GithubIcon />}
+          description="개인 프로젝트 혹은 팀 프로젝트를 저장하는 깃허브 계정입니다."
+        />
+        <SocialLink
+          to="https://aroundthistime.tistory.com/"
+          title="Tech Blog"
+          iconElement={<BlogIcon />}
+          description="개발 관련 학습 내용을 정리하는 블로그입니다."
+        />
       </ul>
     </Section.Content>
   </Section>
-);
-
-const GithubLink = () => (
-  <SocialLink to="https://github.com/aroundthistime">
-    <SocialLink.Default>
-      Github
-      <GithubIcon className="social-link__icon" />
-    </SocialLink.Default>
-    <SocialLink.OnHover>
-      개인 프로젝트 혹은 팀 프로젝트를 저장하는 깃허브 계정입니다.
-    </SocialLink.OnHover>
-  </SocialLink>
-);
-
-const BlogLink = () => (
-  <SocialLink to="https://aroundthistime.tistory.com/">
-    <SocialLink.Default>
-      Tech Blog
-      <BlogIcon className="social-link__icon" />
-    </SocialLink.Default>
-    <SocialLink.OnHover>
-      개발 관련 학습 내용을 정리하는 블로그입니다.
-    </SocialLink.OnHover>
-  </SocialLink>
 );
 
 export default SocialLinks;
