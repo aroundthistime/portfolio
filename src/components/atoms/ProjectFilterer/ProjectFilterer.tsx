4582ec64f814a1331b150416226ed4e0dae34a36
aroundthistime <62099715+aroundthistime@users.noreply.github.com>
2022-03-06 00:50:47 +0900

    project filterer 완료

--- src/components/atoms/ProjectFilterer/ProjectFilterer.tsx
+++ src/components/atoms/ProjectFilterer/ProjectFilterer.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
+import {RootState} from '../../../modules/root';
 import {ProjectFiltererType} from '../../partials/Projects/useProjects';
 import './projectFilterer.scss';
 
@@ -6,15 +8,21 @@ type Props = {
   filterer: ProjectFiltererType;
 };
 
-const ProjectFilterer = ({filterer}: Props) => (
-  <li className="project-filterer">
-    <button
-      className="project-filterer__button"
-      onClick={filterer.use}
-      type="button">
-      {filterer.method}
-    </button>
-  </li>
-);
+const ProjectFilterer = ({filterer}: Props) => {
+  const currentFilterMethod = useSelector(
+    (state: RootState) => state.projectFilterMethod,
+  );
+  const isSelected = filterer.method === currentFilterMethod;
+  return (
+    <li className={`project-filterer ${isSelected ? 'selected' : ''}`}>
+      <button
+        className="project-filterer__button"
+        onClick={filterer.use}
+        type="button">
+        {filterer.method}
+      </button>
+    </li>
+  );
+};
 
 export default ProjectFilterer;
