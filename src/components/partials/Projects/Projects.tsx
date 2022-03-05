17015bf81037c4f913c4078b69a3436c3379b86f
aroundthistime <62099715+aroundthistime@users.noreply.github.com>
2022-03-05 17:55:42 +0900

    firestore setup 완료

--- src/components/partials/Projects/Projects.tsx
+++ src/components/partials/Projects/Projects.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import {collection, getDocs} from 'firebase/firestore';
+import React, {useEffect} from 'react';
+import {firestore} from '../../../firebase';
 import Section from '../Section/Section';
 import './projects.scss';
 
-const Projects = () => (
-  <Section>
-    <Section.Title>Projects</Section.Title>
-    <Section.Content>
-      <ul className="projects" />
-    </Section.Content>
-  </Section>
-);
+const Projects = () => {
+  const getData = async () => {
+    console.log('!');
+    const data: any[] = [];
+    const snapshot = await getDocs(collection(firestore, 'project'));
+    snapshot.forEach(s => data.push(s.data()));
+    console.log(data);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <Section>
+      <Section.Title>Projects</Section.Title>
+      <Section.Content>
+        <ul className="projects" />
+      </Section.Content>
+    </Section>
+  );
+};
 
 export default Projects;
