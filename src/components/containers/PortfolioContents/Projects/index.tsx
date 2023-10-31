876dd525691ebbf0fadf4031c6a41fb72269acdd
Donghwan-VIRNECT <dongdhy@naver.com>
2023-10-31 16:30:41 +0100

    feat: fill project list brief version)

--- src/components/containers/PortfolioContents/Projects/index.tsx
+++ src/components/containers/PortfolioContents/Projects/index.tsx
@@ -1,16 +1,46 @@
 import { SectionTitle } from '@/types/enums/SectionTitle';
 import PortfolioContentBox from '../Templates/PortfolioContentBox';
 import PortfolioSection from '../Templates/PortfolioSection';
+import NestedList, { MultiDepthData } from '@/components/NestedList';
+import LogoWithText from '@/components/LogoWithText';
 
 /**
  * Section for showing the previous projects that I've done
  */
 const Projects = () => {
+  const projects: MultiDepthData[] = [
+    {
+      title: (
+        <LogoWithText
+          text="SQUARS"
+          logoSrc="/images/projects/squars/logo.jpeg"
+        />
+      ),
+      items: ['WebAR platform with online editor & AR Viewer'],
+    },
+    {
+      title: (
+        <LogoWithText text="TRACK" logoSrc="/images/projects/track/logo.jpeg" />
+      ),
+      items: ['Cross-platform framework for AR projects'],
+    },
+    {
+      title: (
+        <LogoWithText text="Owin" logoSrc="/images/projects/owin/logo.jpeg" />
+      ),
+      items: [
+        'Mobile POS application for shop owners of Owin In-car payment service',
+      ],
+    },
+  ];
+
   return (
     <PortfolioSection sectionTitle={SectionTitle.Projects}>
       <PortfolioContentBox>
         <PortfolioContentBox.Header>Projects</PortfolioContentBox.Header>
-        <PortfolioContentBox.Body />
+        <PortfolioContentBox.Body>
+          <NestedList multiDepthDataList={projects} />
+        </PortfolioContentBox.Body>
       </PortfolioContentBox>
     </PortfolioSection>
   );
