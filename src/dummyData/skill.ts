2e89ec5af37f49406edacf6ebc89b072863fb406
Donghwan-VIRNECT <dongdhy@naver.com>
2023-11-01 16:50:33 +0100

    setting: Create data for skills (tools)

---
+++ src/dummyData/skill.ts
@@ -0,0 +1,78 @@
+export const SKILLS = {
+  typescript: {
+    name: 'Typescript',
+    logoSrc: '/images/skills/typescript.png',
+  },
+  styledComponents: {
+    name: 'styled-components',
+    logoSrc: '/images/skills/styledcomponents.png',
+  },
+  scss: {
+    name: 'Scss',
+    logoSrc: '/images/skills/scss.png',
+  },
+  reactJS: {
+    name: 'React JS',
+    logoSrc: '/images/skills/React.png',
+  },
+  reactNative: {
+    name: 'React Native',
+    logoSrc: '/images/skills/React.png',
+  },
+  nextJS: {
+    name: 'Next.js',
+    logoSrc: '/images/skills/Nextjs.png',
+  },
+  reactQuery: {
+    name: 'React Query',
+    logoSrc: '/images/skills/reactquery.png',
+  },
+  redux: {
+    name: 'Redux',
+    logoSrc: '/images/skills/redux.svg',
+  },
+  zustand: {
+    name: 'Zustand',
+    logoSrc: '/images/skills/zustand.png',
+  },
+  threeJS: {
+    name: 'Three.js',
+    logoSrc: '/images/skills/threejs.png',
+  },
+  r3f: {
+    name: 'React Three Fiber',
+    logoSrc: '/images/skills/threejs.png',
+  },
+  codeIgniter: {
+    name: 'PHP (Code Igniter)',
+    logoSrc: '/images/skills/codeIgniter.png',
+  },
+  zeplin: {
+    name: 'Zeplin',
+    logoSrc: '/images/skills/zeplin.png',
+  },
+  webAssembly: {
+    name: 'WebAssembly',
+    logoSrc: '/images/skills/webAssembly.png',
+  },
+  python: {
+    name: 'Python',
+    logoSrc: '/images/skills/python.png',
+  },
+  grafana: {
+    name: 'Grafana',
+    logoSrc: '/images/skills/grafana.png',
+  },
+  cpp: {
+    name: 'C++',
+    logoSrc: '/images/skills/cpp.png',
+  },
+  teamcity: {
+    name: 'Teamcity',
+    logoSrc: '/images/skills/teamcity.png',
+  },
+  figma: {
+    name: 'Figma',
+    logoSrc: '/images/skills/figma.png',
+  },
+} as const;
