2ad42a1bb7abb3653e0aa61e0fbf2e4d8035a12f
Donghwan-VIRNECT <dongdhy@naver.com>
2023-11-01 19:31:30 +0100

    html: Setup overall layout of project page

--- pages/project/[projectUUID]/index.tsx
+++ pages/project/[projectUUID]/index.tsx
@@ -2,9 +2,21 @@ import { GetServerSideProps } from 'next';
 import { PROJECTS } from '@/dummyData/project';
 import { Project } from '@/types/Project';
 import { normalizeURLParam } from '@/utils/url';
+import { ProjectPageContainer, ProjectTitle } from './style';
+
+interface Props {
+  /**
+   * Data of the project with given UUID
+   */
+  project: Project;
+}
 
 const ProjectPage = ({ project }: Props) => {
-  return null;
+  return (
+    <ProjectPageContainer>
+      <ProjectTitle>{project.title}</ProjectTitle>
+    </ProjectPageContainer>
+  );
 };
 
 export const getServerSideProps = (async ({ params }) => {
@@ -18,12 +30,5 @@ export const getServerSideProps = (async ({ params }) => {
     },
   };
 }) satisfies GetServerSideProps<Props>;
-
-interface Props {
-  /**
-   * Data of the project with given UUID
-   */
-  project: Project;
-}
 
 export default ProjectPage;
---
+++ pages/project/[projectUUID]/style.ts
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+
+export const ProjectPageContainer = styled.main`
+  width: 100vw;
+  width: 100dvw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: ${props => {
+    const { vertical, horizontal } = props.theme.layout.padding;
+
+    return `${vertical.xLarge} ${horizontal.xLarge}`;
+  }};
+  line-height: 1.3;
+  row-gap: ${props => props.theme.layout.margin.vertical.large};
+`;
+
+export const ProjectTitle = styled.h1`
+  font-size: ${props => props.theme.font.size.xxLarge};
+  font-weight: 700;
+  font-family: 'Playfair Display', serif;
+  width: 100%;
+`;
+
+export const ProjectSection = styled.section`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  row-gap: ${props => props.theme.layout.margin.vertical.normal};
+`;
+
+export const ProjectSectionTitle = styled.h3`
+  font-family: 'Playfair Display', serif;
+  font-weight: 500;
+  font-size: ${props => props.theme.font.size.xLarge};
+  font-style: italic;
+  padding: ${props => {
+    const { horizontal, vertical } = props.theme.layout.padding;
+
+    return `${vertical.small} ${horizontal.normal}`;
+  }};
+  border-bottom: 1.5px solid rgba(0, 0, 0, 0.1);
+`;
+
+export const ProjectSectionContent = styled.div`
+  padding: 0 ${props => props.theme.layout.padding.horizontal.normal};
+`;
+
+export const ProjectSummaryItem = styled.p``;
