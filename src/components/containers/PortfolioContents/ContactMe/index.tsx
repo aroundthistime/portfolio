86330f0d585b02f5d8287f1175eaeb9ccf9ee219
Donghwan-VIRNECT <dongdhy@naver.com>
2023-11-05 19:00:35 +0100

    refactor: Extract my contact links to constants

--- src/components/containers/PortfolioContents/ContactMe/index.tsx
+++ src/components/containers/PortfolioContents/ContactMe/index.tsx
@@ -7,17 +7,20 @@ import LinkedInLink from '@/utils/link/LinkGenerator/LinkedInLink';
 import NestedList from '@/components/NestedList';
 import EmailLink from '@/utils/link/LinkGenerator/EmailLink';
 import { Link } from '@/types/Link';
+import {
+  MY_EMAIL_LINK,
+  MY_GITHUB_LINK,
+  MY_LINKEDIN_LINK,
+} from '@/constants/links';
 
 /**
  * Section for showing my contacts (eg. social links)
  */
 const ContactMe = () => {
   const CONTACTS: Link[] = [
-    new GithubLink('https://github.com/aroundthistime'),
-    new LinkedInLink(
-      'https://www.linkedin.com/in/%EB%8F%99%ED%99%98-%EC%9C%A0-309910274/',
-    ),
-    new EmailLink('dongdhy@naver.com'),
+    new GithubLink(MY_GITHUB_LINK),
+    new LinkedInLink(MY_LINKEDIN_LINK),
+    new EmailLink(MY_EMAIL_LINK),
   ];
 
   const contactsInMultiDepth = CONTACTS.map(contact => ({
---
+++ src/constants/links.ts
@@ -0,0 +1,4 @@
+export const MY_GITHUB_LINK = 'https://github.com/aroundthistime';
+export const MY_LINKEDIN_LINK =
+  'https://www.linkedin.com/in/%EB%8F%99%ED%99%98-%EC%9C%A0-309910274/';
+export const MY_EMAIL_LINK = 'dongdhy@naver.com';
