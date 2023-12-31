e98783c2dc1fc20ec88dbda0455540210d9fca6e
aroundthistime <62099715+aroundthistime@users.noreply.github.com>
2024-01-01 01:52:28 +0900

    fix: Remove lazy video loading in project detail page (iframe compatibility issue)

--- pages/project/[projectUUID]/ProjectScreenshots/index.tsx
+++ pages/project/[projectUUID]/ProjectScreenshots/index.tsx
@@ -1,6 +1,4 @@
 import { useTranslation } from 'next-i18next';
-import React, { useRef } from 'react';
-import { useIntersectionObserver } from 'usehooks-ts';
 import { Project } from '@/types/Project';
 import ProjectSection from '../ProjectSection';
 import { ProjectScreenshotGroupsContainer } from './style';
@@ -12,30 +10,17 @@ import ProjectScreenshotGroup from './ProjectScreenshotGroup';
 const ProjectScreenshots = ({ screenshotGroups }: Props) => {
   const { t } = useTranslation('projectPage');
 
-  const screenshotsContainerRef = useRef<HTMLUListElement>(null!);
-
-  const shouldRenderRef = useRef<boolean>(false);
-
-  const intersection = useIntersectionObserver(screenshotsContainerRef, {});
-
-  if (intersection?.isIntersecting) {
-    shouldRenderRef.current = true;
-  }
   return (
     <ProjectSection>
       <ProjectSection.Title>{t('screenshots')}</ProjectSection.Title>
       <ProjectSection.Content>
-        <ProjectScreenshotGroupsContainer ref={screenshotsContainerRef}>
-          {shouldRenderRef.current && (
-            <>
-              {screenshotGroups.map(screenshotGroup => (
-                <ProjectScreenshotGroup
-                  screenshotGroup={screenshotGroup}
-                  key={screenshotGroup.title}
-                />
-              ))}
-            </>
-          )}
+        <ProjectScreenshotGroupsContainer>
+          {screenshotGroups.map(screenshotGroup => (
+            <ProjectScreenshotGroup
+              screenshotGroup={screenshotGroup}
+              key={screenshotGroup.title}
+            />
+          ))}
         </ProjectScreenshotGroupsContainer>
       </ProjectSection.Content>
     </ProjectSection>
