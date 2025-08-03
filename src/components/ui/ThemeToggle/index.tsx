41bfa4b4b5d6a7f7a9131a03d4c5dbe9529730ed
aroundthistime <dongdhy@naver.com>
2025-08-03 16:01:53 +0900

    fix: properly detect whether current mode is dark or not when theme is system
    - AS-IS: toggle dark/light theme button didn't work at first click. It does work properly on the following click events
    - Cause of the issue: The default value theme was 'system' as set on the ThemeProvider. This doesn't seem to properly update the theme value to "dark" or "light" mode, but rather maintain the value as "system" while rendering in the system's settings. Therefore, the original onClick handle always changes theme to 'light' which doesn't seem to have effect on already light mode browsers.
    - TO-BE: if theme is not defined or decided to follow system, use window.matchMedia to properly define which mode is applied at the moment, and then toggle the mode into opposite one.

--- src/components/ui/ThemeToggle/index.tsx
+++ src/components/ui/ThemeToggle/index.tsx
@@ -17,11 +17,27 @@ export function ThemeToggle() {
     return null;
   }
 
+  const toggleDarkLightMode = () => {
+    const getCurrentDarkLightMode = () => {
+      if (!theme || theme === 'system') {
+        return window.matchMedia &&
+          window.matchMedia('(prefers-color-scheme: dark)').matches
+          ? 'dark'
+          : 'light';
+      }
+
+      return theme;
+    };
+
+    const currentDarkLightMode = getCurrentDarkLightMode();
+    setTheme(currentDarkLightMode === 'dark' ? 'light' : 'dark');
+  };
+
   return (
     <Button
       variant="ghost"
       size="sm"
-      onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+      onClick={toggleDarkLightMode}
       className="w-9 px-0 cursor-pointer">
       <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
