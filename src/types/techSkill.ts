dde851b90cd1811634e4fdbdf0a7e67f5627b0a6
aroundthistime <dongdhy@naver.com>
2025-08-06 01:32:00 +0900

    feat: enable adding group type for tech skills

--- src/types/techSkill.ts
+++ src/types/techSkill.ts
@@ -1,4 +1,20 @@
+const TECH_SKILL_GROUPS = {
+  languagesAndFrameworks: 'Languages & Frameworks',
+  stateManagement: 'State Management',
+  styling: 'Styling & UI',
+  testing: 'Testing',
+  database: 'Database',
+  infrastructure: 'Devops & Infrastructure',
+  threeD: '3D & Web Graphics',
+  packageManagement: 'Package Management',
+  buildTools: 'Build Tools',
+  tools: 'Working Tools',
+}
+
+export type TechSkillGroup = keyof typeof TECH_SKILL_GROUPS;
+
 export interface TechSkill {
   name: string;
   iconUrl?: string;
+  group?: TechSkillGroup;
 }
