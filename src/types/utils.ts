1a26785831585fdb4673f407bcdaa828b6ba5492
aroundthistime <dongdhy@naver.com>
2025-08-07 22:57:32 +0900

    feat: add util Object methods that are type friendly

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,2 +1,14 @@
 export type BuildTuple<L extends number, T extends unknown[] = []> =
   T['length'] extends L ? T : BuildTuple<L, [...T, unknown]>;
+
+export const getTypedObjectKeys = <T extends object>(obj: T) => {
+  return Object.keys(obj) as Array<keyof T>;
+};
+
+export const getTypedObjectValues = <T extends object>(obj: T) => {
+  return Object.values(obj) as Array<T[keyof T]>;
+};
+
+export const getTypedObjectEntries = <T extends object>(obj: T) => {
+  return Object.entries(obj) as Array<[keyof T, T[keyof T]]>;
+};
