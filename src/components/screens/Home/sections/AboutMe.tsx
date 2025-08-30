7c9c266e0db30e80133e823f41e75041deb64bf0
aroundthistime <dongdhy@naver.com>
2025-08-30 20:36:22 +0900

    content: update my description (omit my career mention)

--- src/components/screens/Home/sections/AboutMe.tsx
+++ src/components/screens/Home/sections/AboutMe.tsx
@@ -24,8 +24,8 @@ const AboutMeSection = () => {
             <CardContent className="pt-8 pb-8 px-8">
               <div className="prose max-w-none text-gray-700 dark:text-gray-300 leading-relaxed flex flex-col gap-6">
                 <p className="text-lg">
-                  안녕하세요, <strong>만 3년차 프론트엔드</strong> 개발자
-                  유동환입니다. 기능 구현을 넘어서&nbsp; 저는{' '}
+                  안녕하세요, 프론트엔드 개발자 유동환입니다. 기능 구현을
+                  넘어서&nbsp;
                   <strong>제품 전반에 깊이 관여</strong>하며, 새로운 아이디어
                   제안이나 기존 기능 개선에도 적극적으로 참여하는 것을
                   좋아합니다. 사용자의 경험을 개선하기 위해&nbsp;
@@ -40,8 +40,9 @@ const AboutMeSection = () => {
                   선호합니다. 큰 기능은 적절히 나누고,&nbsp;
                   <strong>리소스나 병목을 고려해 점진적으로 고도화</strong>해
                   나가는 방식을 자주 사용합니다. 기획, 디자인, 백엔드 등 다양한
-                  직군과의 <strong>협업과 소통</strong>을 즐기며, 여러 시각에서
-                  함께 문제를 정의하고 해결하는 과정을 중요하게 여깁니다.
+                  직군과의 협업과 소통을 즐기며,{' '}
+                  <strong>여러 시각에서 함께 문제를 정의</strong>하고 해결하는
+                  과정을 중요하게 여깁니다.
                 </p>
                 <p className="text-lg">
                   <strong>협업하기 좋은 코드</strong>를 작성하는 데 집중합니다.
@@ -52,8 +53,8 @@ const AboutMeSection = () => {
                   추구합니다. 최근에는 품질 보장을 위해{' '}
                   <strong>자동화된 E2E 테스트</strong>에 힘쓰고 있습니다.&nbsp;
                   <strong>자율성과 책임</strong>이 공존하고,&nbsp;
-                  <strong>피드백을 자유롭게 주고받을 수 있는 팀</strong>을
-                  지향합니다.
+                  <strong>피드백을 자유롭게 주고받을 수 있는 팀</strong>에서
+                  함께 성장하고 싶습니다.
                 </p>
               </div>
             </CardContent>
